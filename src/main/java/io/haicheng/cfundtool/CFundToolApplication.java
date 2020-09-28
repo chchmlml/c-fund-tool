1546d14657232e73f98a423bce8896e6432b2761
haicheng <haicheng@staff.weibo.com>
2020-09-28 10:45:27 +0800

    init

---
+++ src/main/java/io/haicheng/cfundtool/CFundToolApplication.java
@@ -0,0 +1,13 @@
+package io.haicheng.cfundtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CFundToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CFundToolApplication.class, args);
+    }
+
+}
