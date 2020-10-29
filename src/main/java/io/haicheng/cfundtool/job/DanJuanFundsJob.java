3f7257109664014149241d41ce1718c4ee4d6fa4
haicheng <haicheng@staff.weibo.com>
2020-10-29 22:12:32 +0800

    open job

--- src/main/java/io/haicheng/cfundtool/job/DanJuanFundsJob.java
+++ src/main/java/io/haicheng/cfundtool/job/DanJuanFundsJob.java
@@ -20,8 +20,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
- * <p>Title: DanJuanFundsJob</p>
- * <p>Description: </p>
+ * <p>
+ * Title: DanJuanFundsJob
+ * </p>
+ * <p>
+ * Description:
+ * </p>
  *
  * @author haicheng
  * @Email haicheng@staff.weibo.com
@@ -30,7 +34,6 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class DanJuanFundsJob {
-
 
     @Autowired
     FundService fundService;
@@ -44,7 +47,7 @@ public class DanJuanFundsJob {
     @Scheduled(cron = "* */60  * * * ?")
     public void cronJob() {
         log.info(">>>>>>>>>>>>>>>>>>> 蛋卷基金数据 @{}", DateTimeUtil.getCurrentDateTimeStr());
-//        run();
+        run();
     }
 
     private void run() {
@@ -52,35 +55,37 @@ public class DanJuanFundsJob {
         if (response.getResultCode().equals(0)) {
             List<Valuations> valuations = response.getData().getValuations();
             valuations.forEach(v -> {
-                //更新基金
+                // 更新基金
                 if (null != v.getOutsideFund()) {
 
                     Fund fundResponse = fundService.getFundByOutsideFund(v.getOutsideFund());
                     if (null == fundResponse) {
-                        DanjuanResponse<DanjuanResponseFund> responseFund = DanjuanApi.getFundDetail(
-                                v.getOutsideFund());
+                        DanjuanResponse<DanjuanResponseFund> responseFund = DanjuanApi
+                                .getFundDetail(v.getOutsideFund());
 
                         if (responseFund.getResultCode().equals(0)) {
                             Fund fund = new Fund();
-                            fund.setFundName((null != responseFund.getData().getFdFullName()) ? responseFund.getData()
-                                    .getFdFullName() : "");
+                            fund.setFundName((null != responseFund.getData().getFdFullName())
+                                    ? responseFund.getData().getFdFullName()
+                                    : "");
                             fund.setOutsideFund((null != v.getOutsideFund()) ? v.getOutsideFund() : "");
                             fund.setInsideFund((null != v.getInsideFund()) ? v.getInsideFund() : "");
-                            fund.setBuildDate((null != responseFund.getData().getFoundDate()) ? responseFund.getData()
-                                    .getFoundDate() : "");
-                            DanjuanResponse<DanjuanResponseFundDesc> responseFundDesc = DanjuanApi.getFundDetailDesc(
-                                    v.getOutsideFund());
+                            fund.setBuildDate((null != responseFund.getData().getFoundDate())
+                                    ? responseFund.getData().getFoundDate()
+                                    : "");
+                            DanjuanResponse<DanjuanResponseFundDesc> responseFundDesc = DanjuanApi
+                                    .getFundDetailDesc(v.getOutsideFund());
                             fundService.save(fund);
                         }
                     }
                 }
 
-                //更新指数
+                // 更新指数
                 Index index = new Index();
                 index.setName(v.getIndexName());
                 index.setCode(v.getIndexCode());
                 indexService.saveByCode(index);
-                //更新指数数据
+                // 更新指数数据
                 IndexDailyReport report = new IndexDailyReport();
                 if (null != response.getData().getTime()) {
                     report.setDate(response.getData().getTime());
