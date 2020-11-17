0600407c26bd1481f870c2133d9827e8d7ea19a6
haicheng <haicheng@staff.weibo.com>
2020-11-17 12:36:00 +0800

    评分大数逻辑

--- src/main/java/io/haicheng/cfundtool/job/XueqiuJob.java
+++ src/main/java/io/haicheng/cfundtool/job/XueqiuJob.java
@@ -74,15 +74,17 @@ public class XueqiuJob {
                     stockSave.setIa(stock.getIa());
                     stockSave.setTa(stock.getTa());
                     stockSave.setGoodwill(stock.getGoodwill());
-                    Double pbScore;
-                    Double peScore;
-                    if (Arrays.asList("普钢", "特种钢", "钢加工区域地产", "房地产", "银行", "煤炭开采", "公路", "路桥", "水力发电", "火力发电", "新型电力",
-                            "房地产开发", "高速公路", "钢铁").contains(IndustryName)) {
-                        pbScore = 0.6 / stock.getPb() * 100;
-                        peScore = 6 / stock.getPettm() * 100;
-                    } else {
-                        pbScore = 1 / stock.getPb() * 100;
-                        peScore = 10 / stock.getPettm() * 100;
+                    Double pbScore = 0.00;
+                    Double peScore = 0.00;
+                    if(stock.getPb() > 0 && stock.getPb() < 2 && stock.getPettm() < 20 && stock.getPettm() > 0){
+                        if (Arrays.asList("普钢", "特种钢", "钢加工区域地产", "房地产", "银行", "煤炭开采", "公路", "路桥", "水力发电", "火力发电", "新型电力",
+                                "房地产开发", "高速公路", "钢铁").contains(IndustryName)) {
+                            pbScore = 0.6 / stock.getPb() * 100;
+                            peScore = 6 / stock.getPettm() * 100;
+                        } else {
+                            pbScore = 1 / stock.getPb() * 100;
+                            peScore = 10 / stock.getPettm() * 100;
+                        }
                     }
                     stockSave.setPbScore(pbScore);
                     stockSave.setPeScore(peScore);
